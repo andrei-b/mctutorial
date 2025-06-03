46cca617bbf2ad4cdd9f70ccc68efeb62a44eaee
Andrei Borovskii <andrei.borovsky@gmail.com>
2025-06-03 09:03:28 +0200

    Update main.c

--- code/ch_01/main.c
+++ code/ch_01/main.c
@@ -1,1 +1,17 @@
+#include <avr/io.h>
+#include <util/delay.h>
 
+#define LED_PIN PB5
+
+int main(void) {
+    // Set PB5 (pin 13 on Arduino Uno) as output
+    DDRB |= (1 << LED_PIN);
+
+    while (1) {
+        // Toggle the LED
+        PORTB ^= (1 << LED_PIN);
+
+        // Delay for ~1 second
+        _delay_ms(1000);
+    }
+}
